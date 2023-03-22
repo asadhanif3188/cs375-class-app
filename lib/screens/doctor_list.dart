1bf7d0846450f4246f3cdd76626b72b4913ce514
Asad Hanif <asadhanif3188@gmail.com>
2023-03-22 12:06:46 +0500

    Doctor's list is completed

--- lib/screens/doctor_list.dart
+++ lib/screens/doctor_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:third_application/datastore/doctor_datastore.dart';
 
 class DoctorList extends StatelessWidget {
   const DoctorList({Key? key}) : super(key: key);
@@ -9,7 +10,62 @@ class DoctorList extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Doctor's List"),
       ),
-      body: Center(),
+      body: ListView.builder(
+        itemCount: DoctorDataStore.doctors.length,
+
+        itemBuilder: (context, index) {
+          var doctor = DoctorDataStore.doctors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.only(bottom: 16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(16.0),
+                    child: Image.asset(doctor.photo, height: 100, width: 100, fit: BoxFit.cover),
+                ),
+
+                SizedBox(width: 16,),
+                
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                  children: [
+                    Row(
+                      children: [
+                        Text("Name: "),
+                        Text(doctor.name, style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Text("Specitialization: "),
+                        Text(doctor.spe, style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Text("Fee: Rs."),
+                        Text(doctor.fee.toString(), style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+
+                  ],
+                ),
+              ],
+            ),
+          );
+        }
+
+      ),
     );
   }
 }
